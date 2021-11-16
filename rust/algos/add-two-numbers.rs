0372293dda4a826a80a8ef32a00f7b6cd77636b0
Kira Sotnikoff <s8tn1kv@gmail.com>
2021-11-16 16:06:03 +0300

    Add two numbers task, rust

---
+++ rust/algos/add-two-numbers.rs
@@ -0,0 +1,72 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Copy, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    
+    pub fn extract(l: Option<Box<ListNode>>) -> (Option<i32>, Option<Box<ListNode>>) {
+        match l {
+            Some(n) => {
+                (Some(n.val), n.next)
+            }
+            None => {
+                (None, None)
+            }
+        }
+    }
+    
+    fn add(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>, carry: i32) -> Option<Box<ListNode>> {
+
+        
+        return match (l1, l2) {
+            (Some(l1), Some(l2)) => {
+                let (a, n1) = Self::extract(Some(l1));
+                let (b, n2) = Self::extract(Some(l2));
+                let x = a.unwrap() + b.unwrap() + carry;
+                let mut node = ListNode::new(x % 10);
+                node.next = Self::add(n1, n2, x / 10);
+                Some(Box::new(node))           
+            }
+            (Some(l1), None) => {
+                let (a, n1) = Self::extract(Some(l1));
+                let x = a.unwrap() + carry;
+                let s = if x >= 10 { x % 10} else { x };
+                
+                let mut node = ListNode::new(s);
+                node.next = Self::add(n1, None, x / 10);
+                Some(Box::new(node))
+            }
+             (None, Some(l2)) => {
+                let (b, n2) = Self::extract(Some(l2)); 
+                let x = b.unwrap() + carry;
+                let s = if x >= 10 { x % 10} else { x }; 
+                 
+                let mut node = ListNode::new(s);
+                node.next = Self::add(None, n2, x / 10);
+                Some(Box::new(node))
+            }
+            (None, None) => {
+                if carry != 0 {Some(Box::new(ListNode::new(carry)))} else {None}
+            }
+        };
+    }
+
+    pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> (Option<Box<ListNode>>) {
+            
+        Self::add(l1, l2, 0)
+        
+   }
+}
+    
